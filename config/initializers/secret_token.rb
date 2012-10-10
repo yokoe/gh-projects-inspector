05ae6738a72abd510b08ebda1ffb201f8412a850
Sota <sota@pankaku.com>
2012-10-10 19:58:27 +0900

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GhProjectsInspector::Application.config.secret_token = 'ab263d1a98f74e0a7f064a5c6f31331e997d6d6800c6c3747bf97e3410d56668583519819b1f91c94ae8213d281f934a21a2c97d650796365137c904b2f2922c'
